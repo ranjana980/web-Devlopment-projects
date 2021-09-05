1597ec9a49f8063e92765e6b05c206b23f5b5ff3
Ranjana Chaudhary <you@example.com>
2021-09-05 09:24:40 +0530

    first commit

---
+++ ToDoList-master/q1.py
@@ -0,0 +1,22 @@
+
+# string=input("enter the string")
+# for i in string:
+#     if i in a:
+#         print(string[i])
+#     else:
+#         print(string[i])
+# i=0
+# a=["a","e","i","o","u"]
+# while i<=len(string):
+#     if i in a:
+#         print(string[i],"vowel")
+#     else:
+#         print(string[i],"cosonent")
+#     i=i+1
+string1=input("enter the string")
+for i in string1:
+    if i =="a" or i=="e"or i=="i" or i=="o" or i=="u":
+        print(i,"vowel")
+    else:
+        print(i,"consonent")
+   
